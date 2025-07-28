950898c07c9aec251e554133bbc96872964ed63c
Edgar-PTC <20230280@ricaldone.edu.sv>
2025-07-28 10:53:13 -0600

    Primer commit con el proyecto

---
+++ src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/ProyectoModuloEdgarPineda2023028Application.java
@@ -0,0 +1,13 @@
+package EdgarPineda_20230280.EdgarPineda_20230280;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoModuloEdgarPineda2023028Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoModuloEdgarPineda2023028Application.class, args);
+	}
+
+}
---
+++ src/test/java/EdgarPineda_20230280/EdgarPineda_20230280/ProyectoModuloEdgarPineda2023028ApplicationTests.java
@@ -0,0 +1,13 @@
+package EdgarPineda_20230280.EdgarPineda_20230280;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoModuloEdgarPineda2023028ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
