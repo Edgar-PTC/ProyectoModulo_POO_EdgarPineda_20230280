ace5c57aa8102b1a2e1c16bd8546a23fbd6b9d52
Edgar-PTC <20230280@ricaldone.edu.sv>
2025-07-28 14:46:40 -0600

    YA ME DIO CARAJO

--- src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/Controllers/ProveedoresController.java
+++ src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/Controllers/ProveedoresController.java
@@ -2,10 +2,16 @@ package EdgarPineda_20230280.EdgarPineda_20230280.Controllers;
 
 import EdgarPineda_20230280.EdgarPineda_20230280.Models.DTO.ProveedoresDTO;
 import EdgarPineda_20230280.EdgarPineda_20230280.Services.ProveedoresService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/apiProveedores")
@@ -28,5 +34,30 @@ public class ProveedoresController {
         return service.buscarporID(id);
     }
 
+    //Endpoint para la peticion Post
+    @PostMapping("/postProveedores")
+    public ResponseEntity<?> NuevoProveedores(@Valid @RequestBody ProveedoresDTO json, HttpServletRequest request){
+        try{
+            ProveedoresDTO respuesta = service.insertarProveedor(json);
+            if (respuesta == null){
+                return ResponseEntity.badRequest().body(Map.of(
+                        "status", "Insercion fallida",
+                        "errorType", "VALIDATION_ERROR",
+                        "message", "Los datos no se pudieron insertar"
+                ));
+            }
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                    "status", "success",
+                    "data", respuesta
+            ));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+                    "status", "error",
+                    "message", "error no controlado al registrar nuevo Proveedor",
+                    "detail", e.toString(),
+                    "stackTrace", Arrays.toString(e.getStackTrace())
+            ));
+        }
+    }
 
 }
