f2214202220c0a53ff0885665d7a8ea81524ecbf
Edgar-PTC <20230280@ricaldone.edu.sv>
2025-07-28 11:58:22 -0600

    Entity y DTO listos

--- src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/Entities/ProveedoresEntity.java
+++ src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/Entities/ProveedoresEntity.java
@@ -1,14 +1,42 @@
 package EdgarPineda_20230280.EdgarPineda_20230280.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
+//Tabla a la que pertenece
 @Table(name = "TBPROVIDER")
 @Getter @Setter @ToString @EqualsAndHashCode
 public class ProveedoresEntity {
+
+    //Atributos = Columnas
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_provider")
+    @SequenceGenerator(name = "seq_provider", sequenceName = "seq_provider", allocationSize = 1)
+    @Column(name = "PROVIDERID")
+    private long id;
+
+    @Column(name = "PROVIDERNAME", unique = true)
+    private String nombre;
+
+    @Column(name = "PROVIDERPHONE")
+    private int telefono;
+
+    @Column(name = "PROVIDERADDRESS")
+    private String address;
+
+    @Column(name = "PROVIDEREMAIL")
+    private String email;
+
+    @Column(name = "PROVIDERCODE", unique = true)
+    private long code;
+
+    @Column(name = "PROVIDERSTATUS")
+    private int status;
+
+    @Column(name = "PROVIDERCOMMENTS")
+    private String comment;
 }
--- src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/Models/DTO/ProveedoresDTO.java
+++ src/main/java/EdgarPineda_20230280/EdgarPineda_20230280/Models/DTO/ProveedoresDTO.java
@@ -1,5 +1,9 @@
 package EdgarPineda_20230280.EdgarPineda_20230280.Models.DTO;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +12,29 @@ import lombok.ToString;
 @ToString @EqualsAndHashCode
 @Getter @Setter
 public class ProveedoresDTO {
+    private long id;
 
+    @NotBlank(message = "El nombre es obligatorio y unico")
+    @Size(max = 50)
+    private String nombre;
 
+    @Getter @Setter @Size(max = 25)
+    private int telefono;
 
+    @Size(max = 128)
+    private String address;
+
+    @NotBlank @Email(message = "Debe tener formato de correo electronico")
+    @Size(max = 100)
+    private String email;
+
+    @Getter @Setter
+    @Size(max = 35)
+    private long code;
+
+    @Getter @Setter
+    private int status;
+
+    @Size(max = 256)
+    private String comment;
 }
